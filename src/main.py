ae6dd8c950371d48eff890dd0c723a7288390096
michael-hoang <mike081522@gmail.com>
2023-02-18 22:10:41 -0800

    Refactor main.py to OOP

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import tkinter as tk
+from tkinter import messagebox
 from PIL import Image, ImageTk
 from wrapup import WrapUpDateCalculator
 from cardpayment import CardPayment
 from map import Map
 from pwmanager import PasswordManager
 from authentication import Authenticator
+import os
+import sys
+import json
+import requests
 
 
 FONT = ('Bahnschrift Light', 17, 'normal')
@@ -14,105 +19,128 @@ HOVER_BUTTON_COLOR = 'SlateGray4'
 ACTIVE_BG_COLOR = 'SlateGray2'
 
 
-def open_WrapUpDateCalculator():
-    """Instantiate WrapUpDateCalculator object in a new TopLevel window."""
-    top = WrapUpDateCalculator()
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.withdraw()
+        self.title('Project AtBS')
+        self.config(bg=BG_COLOR)
+        self.resizable(width=False, height=False)
+        self.iconphoto(False, tk.PhotoImage(file='assets/img/atbs_icon.png'))
+
+        self.button_images = []
+        # Create buttons
+        self.cp_but = self.create_button(
+            'Payment', 'cc_icon.png', self.open_CardPaymentForm)
+        self.wud_but = self.create_button(
+            'Wrap Up', 'cal_calc.png', self.open_WrapUpDateCalculator)
+        self.m_but = self.create_button(
+            'Map', 'map_icon.png', self.open_MapSearch)
+        self.pm_but = self.create_button(
+            'Password', 'lock_icon.png', self.open_PasswordManager)
+
+        # Place buttons on grid
+        self.cp_but.grid(column=0, row=0, sticky='EW')
+        self.wud_but.grid(column=0, row=1, sticky='EW')
+        self.m_but.grid(column=0, row=2, sticky='EW')
+        self.pm_but.grid(column=0, row=3, sticky='EW')
+
+        # Bind events
+        self.bind('<Enter>', self.pointerEnter)
+        self.bind('<Leave>', self.pointerLeave)
+
+        # Center window to screen
+        self.update_idletasks()
+        win_width = self.winfo_reqwidth()
+        win_height = self.winfo_reqheight()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = int(screen_width/2 - win_width/2)
+        y = int(screen_height/2 - win_width/2)
+        self.geometry(f"{win_width}x{win_height}+{x}+{y}")
+        self.deiconify()
+
+    def create_button(self, text, image_path, command):
+        img_open = Image.open(fp=f'assets/img/{image_path}')
+        img_resized = img_open.resize(size=(50, 50))
+        self.img = ImageTk.PhotoImage(img_resized)
+        self.button_images.append(self.img)
+        button = tk.Button(text=f'  {text}', image=self.img, compound='left', font=FONT, fg=FG_COLOR,
+                           bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR, command=command,
+                           anchor='w', padx=10, pady=5, width=207)
+        return button
+
+    def open_WrapUpDateCalculator(self):
+        """Instantiate WrapUpDateCalculator object in a new TopLevel window."""
+        WrapUpDateCalculator()
+
+    def open_CardPaymentForm(self):
+        """Instantiate CardPayment object in a new TopLevel window."""
+        CardPayment()
+
+    def open_MapSearch(self):
+        """Instantiate Map object in a new TopLevel window."""
+        Map()
+
+    def open_PasswordManager(self):
+        """Instantiate Password Manager in a new TopLevel window."""
+        a = Authenticator()
+        self._check_isVerified_flag(a)
+
+    def _check_isVerified_flag(self, authenticator):
+        """Recursively checks isVerified flag to signal instantiation of Password Manager object."""
+        if not authenticator.isVerified:
+            self.after(50, self._check_isVerified_flag, authenticator)
+        else:
+            authenticator.top.destroy()
+            PasswordManager()
+
+    def pointerEnter(self, event):
+        """Change button color on mouse hover."""
+        event.widget['bg'] = HOVER_BUTTON_COLOR
+
+    def pointerLeave(self, event):
+        """Change button color back to normal when mouse leave button."""
+        event.widget['bg'] = BG_COLOR
 
 
-def open_CardPaymentForm():
-    """Instantiate CardPayment object in a new TopLevel window."""
-    top = CardPayment()
+def check_for_updates() -> bool:
+    """Check if new version of Main App or App Update Manager is available."""
 
-
-def open_MapSearch():
-    """Instantiate Map object in a new TopLevel window."""
-    top = Map()
-
-
-def open_PasswordManager():
-    """Instantiate Password Manager in a new TopLevel window."""
-    a = Authenticator()
-
-    _check_isVerified_flag(a)
-
-
-def _check_isVerified_flag(authenticator):
-    """Recursively checks isVerified flag to signal instantiation of Password Manager object."""
-    if not authenticator.isVerified:
-        root.after(50, _check_isVerified_flag, authenticator)
+    if getattr(sys, 'frozen', False):
+        root_path = os.path.dirname(sys.executable)
     else:
-        authenticator.top.destroy()
-        pm = PasswordManager()
+        root_path = os.path.dirname(os.path.abspath(__file__))
+
+    current_version_path = f'{root_path}\dist\current_version\current_version.json'
+    latest_version_url = 'https://raw.githubusercontent.com/michael-hoang/project-atbs-work/main/dist/latest_version/latest_version.json'
+    with open(current_version_path) as f:
+        data = json.load(f)
+        main_app_current_version = data['main']
+
+    latest_version_response = requests.get(latest_version_url)
+    if latest_version_response.status_code == 200:
+        data = json.loads(latest_version_response.content)
+        main_app_latest_version = data['main']
+
+    if main_app_current_version != main_app_latest_version:
+        return messagebox.askyesno(title='New Update Available',
+                                   message=f'{main_app_latest_version} is now available. Do you want to open App Update Manager?')
+    return False
 
 
-def pointerEnter(event):
-    """Change button color on mouse hover."""
-    event.widget['bg'] = HOVER_BUTTON_COLOR
+def open_Updater():
+    """Run App Update Manager"""
+
+    if getattr(sys, 'frozen', False):
+        root_path = os.path.dirname(sys.executable)
+    else:
+        root_path = os.path.dirname(os.path.abspath(__file__))
+    os.startfile(f'{root_path}/dist/update.exe')
 
 
-def pointerLeave(event):
-    """Change button color back to normal when mouse leave button."""
-    event.widget['bg'] = BG_COLOR
-
-
-root = tk.Tk()
-root.withdraw()
-root.title('Project AtBS')
-root.config(bg=BG_COLOR)
-root.resizable(width=False, height=False)
-
-atbs_icon = tk.PhotoImage(file="assets/img/atbs_icon.png")
-root.iconphoto(False, atbs_icon)
-
-# Card payment form
-cc_img_open = Image.open(fp='assets/img/cc_icon.png')
-cc_img_resized = cc_img_open.resize(size=(50, 50))
-cc_img = ImageTk.PhotoImage(cc_img_resized)
-cp_but = tk.Button(text='  Payment', image=cc_img, compound='left', font=FONT, fg=FG_COLOR,
-                   bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR, command=open_CardPaymentForm,
-                   anchor='w', padx=10, pady=5, width=208)
-cp_but.grid(column=0, row=0, sticky='EW')
-
-# Wrap up date calculator
-cal_calc_img_open = Image.open(fp='assets/img/cal_calc.png')
-cal_calc_img_resized = cal_calc_img_open.resize(size=(50, 50))
-cal_calc_img = ImageTk.PhotoImage(cal_calc_img_resized)
-wud_but = tk.Button(text='  Wrap Up', image=cal_calc_img, compound='left', font=FONT,
-                    fg=FG_COLOR, bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR,
-                    command=open_WrapUpDateCalculator, anchor='w', padx=10, pady=5, width=207)
-wud_but.grid(column=0, row=1, sticky='EW')
-
-# Map search
-map_img_open = Image.open(fp='assets/img/map_icon.png')
-map_img_resized = map_img_open.resize(size=(50, 50))
-map_img = ImageTk.PhotoImage(map_img_resized)
-m_but = tk.Button(text='  Map', image=map_img, compound='left', font=FONT, fg=FG_COLOR,
-                  bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR, command=open_MapSearch,
-                  anchor='w', padx=10, pady=5, width=207)
-m_but.grid(column=0, row=2, sticky='EW')
-
-# Password Manager
-lock_img_open = Image.open(fp='assets/img/lock_icon.png')
-lock_img_resized = lock_img_open.resize(size=(50, 50))
-lock_img = ImageTk.PhotoImage(lock_img_resized)
-pm_but = tk.Button(text='  Password', image=lock_img, compound='left', font=FONT,
-                   fg=FG_COLOR, bg=BG_COLOR, activebackground=ACTIVE_BG_COLOR,
-                   command=open_PasswordManager, anchor='w', padx=10, pady=5, width=207)
-pm_but.grid(column=0, row=3, sticky='EW')
-
-# Center window to screen
-root.update_idletasks()
-win_width = root.winfo_reqwidth()
-win_height = root.winfo_reqheight()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = int(screen_width/2 - win_width/2)
-y = int(screen_height/2 - win_width/2)
-root.geometry(f"{win_width}x{win_height}+{x}+{y}")
-root.deiconify()
-# root.attributes('-topmost', 1)
-
-root.bind('<Enter>', pointerEnter)
-root.bind('<Leave>', pointerLeave)
-
-root.mainloop()
+if __name__ == '__main__':
+    if check_for_updates():
+        open_Updater()
+    else:
+        MainApp().mainloop()
